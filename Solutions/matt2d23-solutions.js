b210bd1113336b777fc44bdda8df0ddc580d259a
Matthew Whitfield <matthewwhitfield2000@gmail.com>
2024-01-12 00:05:33 -0600

    More work on completing all the functions

--- Solutions/matt2d23-solutions.js
+++ Solutions/matt2d23-solutions.js
@@ -1,48 +1,48 @@
-// 1. Write a function identity that takes an argument and returns that argument
+// 1. Write a function 'identity' that takes an argument and returns that argument
 
 const identity = (x) => x;
 
-// 2. Write a binary function addb that takes two numbers and returns their sum
+// 2. Write a binary function 'addb' that takes two numbers and returns their sum
 
 const addb = (x, y) => x+y;
 
-// 3. Write a binary function subb that takes two numbers and returns their difference
+// 3. Write a binary function 'subb' that takes two numbers and returns their difference
 
 const subb = (x, y) => x-y;
 
-// 4. Write a binary function mulb that takes two numbers and returns their product
+// 4. Write a binary function 'mulb' that takes two numbers and returns their product
 
 const mulb = (x, y) => x*y;
 
-// 5. Write a binary function minb that takes two numbers and returns the smaller one
+// 5. Write a binary function 'minb' that takes two numbers and returns the smaller one
 
 const minb = (x, y) => x < y ? x : y;
 
-// 6. Write a binary function maxb that takes two numbers and returns the larger one
+// 6. Write a binary function 'maxb' that takes two numbers and returns the larger one
 
 const maxb = (x, y) => x > y ? x : y;
 
-// 7. Write a function add that is generalized for any amount of arguments
+// 7. Write a function 'add' that is generalized for any amount of arguments
 
 const add = (...nums) => nums.reduce((accumulator, currentValue) => accumulator + currentValue);
 
-// 8. Write a function sub that is generalized for any amount of arguments
+// 8. Write a function 'sub' that is generalized for any amount of arguments
 
 const sub = (...nums) => nums.reduce((accumulator, currentValue) => accumulator - currentValue);
 
-// 9. Write a function mul that is generalized for any amount of arguments
+// 9. Write a function 'mul' that is generalized for any amount of arguments
 
 const mul = (...nums) => nums.reduce((accumulator, currentValue) => accumulator * currentValue);
 
-// 10. Write a function min that is generalized for any amount of arguments
+// 10. Write a function 'min' that is generalized for any amount of arguments
 
 const min = (...nums) => nums.reduce((accumulator, currentValue) => accumulator < currentValue ? accumulator : currentValue);
 
-// 11. Write a function max that is generalized for any amount of arguments
+// 11. Write a function 'max' that is generalized for any amount of arguments
 
 const max = (...nums) => nums.reduce((accumulator, currentValue) => accumulator > currentValue ? accumulator : currentValue);
 
-// 12. Write a function addRecurse that is the generalized add function but uses recursion
+// 12. Write a function 'addRecurse' that is the generalized add function but uses recursion
 
 const addRecurse = (...nums) => {
     if (nums.length === 1) {
@@ -52,7 +52,7 @@ const addRecurse = (...nums) => {
     }
 }
 
-// 13. Write a function mulRecurse that is the generalized mul function but uses recursion
+// 13. Write a function 'mulRecurse' that is the generalized mul function but uses recursion
 
 const mulRecurse = (...nums) => {
     if (nums.length === 1) {
@@ -62,7 +62,7 @@ const mulRecurse = (...nums) => {
     }
 }
 
-// 14. Write a function minRecurse that is the generalized min function but uses recursion
+// 14. Write a function 'minRecurse' that is the generalized min function but uses recursion
 
 const minRecurse = (...nums) => {
     if (nums.length === 1) {
@@ -72,7 +72,7 @@ const minRecurse = (...nums) => {
     }
 }
 
-// 15. Write a function maxRecurse that is the generalized min function but uses recursion
+// 15. Write a function 'maxRecurse' that is the generalized min function but uses recursion
 
 const maxRecurse = (...nums) => {
     if (nums.length === 1) {
@@ -82,8 +82,41 @@ const maxRecurse = (...nums) => {
     }
 }
 
+// 16. Write a function 'not' that takes a function and returns the negation of its result
 
+const not = (fn) => {
+    return !fn;
+}
 
+function test() {
+    return true;
+}
+
+console.log(not(test));
+
+// 17. Write a function acc that takes a function and an initial value,
+// and returns a function that runs the initial function on each argument, accumulating the result
+
+function acc(func, initial) {
+    return (...args) => {
+        let result = args.reduce((accumulator, currentValue) => func(accumulator, currentValue), initial);
+        return result;
+    }
+}
+
+function addition(x,y) {
+    return x+y;
+}
+
+function multiply(x,y) {
+    return x*y
+}
+
+let add2 = acc(addition, 0);
+let mult = acc(multiply, 1);
+
+console.log(add2(1,2,4)); // 7
+console.log(mult(1,2,4)); // 8
 
 
 
@@ -105,8 +138,8 @@ module.exports = {
     mulRecurse,
     minRecurse,
     maxRecurse,
-    // not,
-    // acc,
+    not,
+    acc,
     // accPartial,
     // accRecurse,
     // fill,
